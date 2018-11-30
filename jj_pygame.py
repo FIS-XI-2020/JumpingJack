95c5fc086dc4d02523333b309f82b6f98bc30288
Ad!thya R <gh0strider.2k18.reborn@gmail.com>
2018-11-30 07:32:04 +0530

    Fix falling fo stickman on platforms
    Remaining TODO:
    - Score & timer
    - Collision detection

--- jj_pygame.py
+++ jj_pygame.py
@@ -14,13 +14,13 @@ blue = (0, 102, 204)
 # Initialize pygame
 pygame.init()
 pygame.display.set_caption('Jumping Jack')
+window = pygame.display.set_mode((900, 700))
 
 # Lets play some tunes
 pygame.mixer.music.load('res/music.mp3')
 pygame.mixer.music.play(-1)
 
 clock = pygame.time.Clock()
-window = pygame.display.set_mode((900, 700))
 
 """
     Function to create a button with text in it and set its colour to its light
@@ -57,7 +57,6 @@ def display_text(text, place, font, size):
 
 """
     The main menu. Options: Play, High scores, Quit
-    TODO: Playing instructions
 """
 def mainmenu():
     run = True
@@ -81,8 +80,12 @@ def mainmenu():
         pygame.display.update()
         clock.tick(15)
 
+# Set of coordinates for the platform in (x, y) format
 platform_coord = [(291, 567), (463, 504), (572, 453), (739, 443), (409, 377), (206, 328), (61, 273), (257, 189), (405, 180), (567, 122), (636, 252)]
 
+"""
+    Function to redraw background & platforms when updating stickman
+"""
 def redraw():
     bg = pygame.image.load("res/bg_game.png")
     window.blit(bg, (0, 0))
@@ -96,8 +99,7 @@ def redraw():
 def platformcheck(x, y):
     onplatform=False
     for i in platform_coord:
-        #print(x, y+94, i[0], i[1])
-        if (i[1] in range(y+78, y+94)) and (x in range(i[0]-64, i[0]+100)):
+        if (i[1] in range(y+78, y+94)) and (x in range(i[0]-50, i[0]+80)):
             onplatform=True
             break
     return onplatform
@@ -128,6 +130,7 @@ def gameloop():
 
         window.blit(stickman, (x,y))
 
+        # TODO: Collision check (prevent walking throuh platforms)
         collision=False
         """for i in platform_coord:
             if ((x+64 in range(i[0]-11, i[0]+11)) or (x in range(i[0]-11, i[0]+11))) and (i[1] in range(y, y+92)):
@@ -156,16 +159,13 @@ def gameloop():
             velocity=15
             iskeyright=False
             iskeyleft=False
-            for event in pygame.event.get():
-                if event.type==KEYDOWN:
-                    if event.key==K_RIGHT:
-                        iskeyright = True
-                    elif event.key==K_LEFT:
-                        iskeyleft = True
+
             for i in range(0, 29):
                 jumping=True
                 velocity-=1
                 y-=velocity
+
+                # Check if right/left key is being pressed
                 for event in pygame.event.get():
                     if event.type==KEYDOWN:
                         if event.key==K_RIGHT:
@@ -178,6 +178,7 @@ def gameloop():
                 elif x>=50 and iskeyleft:
                     direction="jumping_left"
                     x-=8
+
                 redraw()
                 window.blit(stickman, (x,y))
                 pygame.display.flip()
@@ -187,10 +188,11 @@ def gameloop():
                     break
 
         if not platformcheck(x, y):
-            downvel=5
-            while y<530:
+            downvel=12
+            while y<=520:
+                falling=True
                 y+=downvel
-                downvel+=1
+                if platformcheck(x, y): break
                 redraw()
                 window.blit(stickman, (x,y))
                 pygame.display.flip()
